037fe568e9f4ac018396ebb1a17e1b71fcbf39e8
SamJay0 <samjay0.254@gmail.com>
2020-03-14 14:40:33 +0300

    navigation, visual polish

--- app/src/main/java/com/devjay/tatuatraffic/LoginFragment.java
+++ app/src/main/java/com/devjay/tatuatraffic/LoginFragment.java
@@ -7,12 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class LoginFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
