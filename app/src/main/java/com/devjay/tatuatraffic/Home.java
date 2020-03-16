d86164084d95d8f43d9f4d415a6f7659bbea2b05
SamJay0 <samjay0.254@gmail.com>
2020-03-16 22:58:08 +0300

    get fragment from fragmentState adapter

--- app/src/main/java/com/devjay/tatuatraffic/Home.java
+++ app/src/main/java/com/devjay/tatuatraffic/Home.java
@@ -2,19 +2,34 @@ package com.devjay.tatuatraffic;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.devjay.tatuatraffic.adapters.HomeCollectionAdapter;
+
 
 public class Home extends Fragment {
+
+    HomeCollectionAdapter homeCollectionAdapter;
+    ViewPager2 viewPager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        homeCollectionAdapter = new HomeCollectionAdapter(this);
+        viewPager = view.findViewById(R.id.pager);
+        viewPager.setAdapter(homeCollectionAdapter);
     }
 }
